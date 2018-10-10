beb84b0c13be5856d91a3deac9056e1decb0fe98
ElfanRodh <elfanapoywali@gmail.com>
2018-10-10 21:09:31 +0700

    initial commit

---
+++ sample/src/main/java/com/github/florent37/materialviewpager/sample/TestRecyclerViewAdapter.java
@@ -0,0 +1,87 @@
+package com.github.florent37.materialviewpager.sample;
+
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by florentchampigny on 24/04/15.
+ */
+public class TestRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    List<Object> contents;
+
+    static final int TYPE_HEADER = 0;
+    static final int TYPE_CELL = 1;
+    RecyclerView mRecyclerView;
+
+    public TestRecyclerViewAdapter(List<Object> contents) {
+        this.contents = contents;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        switch (position) {
+            case 0:
+                return TYPE_CELL;
+            default:
+                return TYPE_CELL;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return contents.size();
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = null;
+        TextView txtDeposit, txtSaldo;
+        ImageView imgTk;
+
+        switch (viewType) {
+            case TYPE_HEADER: {
+                view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.list_item_card_big, parent, false);
+                txtDeposit  = view.findViewById(R.id.txtDeposit);
+                txtDeposit.setText("Rp. 50.000");
+                txtSaldo    = view.findViewById(R.id.txtSaldo);
+                txtSaldo.setText("Rp. 100.000");
+                imgTk = view.findViewById(R.id.imgTukang);
+                imgTk.setImageResource(R.drawable.tukangku);
+
+//                mRecyclerView.setLayoutManager(new LinearLayoutManager(parent.getContext()));
+                return new RecyclerView.ViewHolder(view) {
+                };
+            }
+            case TYPE_CELL: {
+                view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.list_item_card_small, parent, false);
+
+//                mRecyclerView.setLayoutManager(new GridLayoutManager(parent.getContext(), 2));
+                return new RecyclerView.ViewHolder(view) {
+                };
+            }
+        }
+        return null;
+    }
+
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        switch (getItemViewType(position)) {
+            case TYPE_HEADER:
+                break;
+            case TYPE_CELL:
+                break;
+        }
+    }
+}
